30e72a28e4952272c6de283d0f7ae03d79a856e2
Kenny <me@jaleung.net>
2018-03-23 01:26:49 +0800

    brief card list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 
-let fetchedData = {
-  name: 'Enigma',
-  thumbnail: 'http://placehold.it/100x100'
+let fetchedData = [{
+  name: "Enigma",
+  thumbnail: "http://placehold.it/100x100"
+}];
+
+const CardList = (props) => {
+  return <div>{props.cards.map(card => <Card key={card.name} {...card}/>)}</div>;
 };
-const Card = (props) => {
+
+const Card = props => {
   return (
     <div className="card">
       <div className="thumbnail">
-        <img src={props.data.thumbnail} alt={props.name} />
+        <img src={props.thumbnail} alt={props.name} />
       </div>
-      <div className="title">{props.data.name}</div>
+      <div className="title">{props.name}</div>
     </div>
   );
 };
@@ -19,7 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Card data={fetchedData}/>
+        <CardList cards={fetchedData} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
