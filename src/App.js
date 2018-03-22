638049fdf4b69a333427ff8d776c1d183c91a4dc
Kenny <me@jaleung.net>
2018-03-23 01:06:56 +0800

    barely working dummy page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
+let fetchedData = {
+  name: 'Enigma',
+  thumbnail: 'http://placehold.it/100x100'
+};
+const Card = (props) => {
+  return (
+    <div className="card">
+      <div className="thumbnail">
+        <img src={props.data.thumbnail} alt={props.name} />
+      </div>
+      <div className="title">{props.data.name}</div>
+    </div>
+  );
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Card data={fetchedData}/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
