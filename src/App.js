e4a41d0a3fbf0599c1fa78cb7d4b1229a05126d6
Kenny Leung <kenny.leung@8securities.com>
2018-03-23 18:24:55 +0800

    working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
-
-let fetchedData = [{
-  name: "Enigma",
-  thumbnail: "http://placehold.it/100x100"
-}];
+import Axios from "axios";
 
 const CardList = (props) => {
-  return <div>{props.cards.map(card => <Card key={card.name} {...card}/>)}</div>;
+  return <div>{props.cards.map(card => <Card key={card.uuid} {...card}/>)}</div>;
 };
 
 const Card = props => {
   return (
     <div className="card">
       <div className="thumbnail">
-        <img src={props.thumbnail} alt={props.name} />
+        <img width="100px" src={props.field_thumbnail} alt={props.title} />
       </div>
-      <div className="title">{props.name}</div>
+      <div className="title">{props.title}</div>
     </div>
   );
 };
 class App extends Component {
+  state = {
+    cards: []
+  }
+  componentDidMount() {
+    Axios.get(`https://35.189.186.209/portfolios?_format=json`).then(
+      resp => {
+        this.setState(prevState => ({
+          cards: prevState.cards.concat(resp.data)
+        }));
+        console.log(this.state.cards);
+      }
+    );
+  }
   render() {
     return (
       <div className="App">
-        <CardList cards={fetchedData} />
+        <CardList cards={this.state.cards} />
       </div>
     );
   }
