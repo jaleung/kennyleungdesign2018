4316c30fea4a86206de2daef3d5ec5454413a344
Kenny <me@jaleung.net>
2018-04-03 22:07:07 +0800

    cleaned up

--- src/App.js
+++ src/App.js
@@ -17,34 +17,6 @@ class App extends Component {
           <MuiThemeProvider theme={myTheme}>
             <div className="App">
               <NavBar />
-              {/* <AppBar position="static" color="default">
-              <Toolbar>
-                <Grid container spacing={8} alignItems="center">
-                  <Grid
-                    item
-                    xs={12}
-                    sm={4}
-                    md={3}
-                    style={{ textAlign: "left" }}
-                  >
-                    <Typography variant="title" color="inherit">
-                      <Link to="/">Kenny Leung | UX Developer</Link>
-                    </Typography>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={12}
-                    sm={8}
-                    md={9}
-                    style={{ textAlign: "right" }}
-                  >
-                    <Link to="/login">
-                      <Button>Login</Button>
-                    </Link>
-                  </Grid>
-                </Grid>
-              </Toolbar>
-            </AppBar> */}
               <AuthBtn />
               <Route path="/" component={CardList} />
               <Route path="/logout" component={Logout} />
