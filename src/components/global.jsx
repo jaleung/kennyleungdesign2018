cf304fa0ab0d88a504ae40dc3fc2b10b281d6f86
Kenny Leung <kenny.leung@8securities.com>
2018-04-09 17:03:03 +0800

    media query

--- src/components/global.jsx
+++ src/components/global.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import Responsive from 'react-responsive';
 
 export const baseUrl = "https://api.kennyleung.design";
 
@@ -13,3 +15,6 @@ export const portfoUrl = url => {
 export const capitalize = function(stringToCap) {
   return stringToCap.charAt(0).toUpperCase() + stringToCap.slice(1);
 }
+
+export const SM = props => <Response {...props} maxWidth={959}/>
+export const MD = props => <Response {...props} minWidth={960}/>
