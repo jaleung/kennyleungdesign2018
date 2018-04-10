01506f579a431585785e92a1c7b553aadd580545
Kenny <me@jaleung.net>
2018-04-11 01:50:05 +0800

    wip

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,82 @@
 import React from "react";
 import ScrollableAnchor from "react-scrollable-anchor";
-
-// import "react-syntax-highlight/lib/style.css";
-import "highlight.js/styles/xcode.css";
+import "../css/mono.css"
+import Scroller from "./scrollToPage";
 
 export const Home = props => {
-  return (
-    <ScrollableAnchor id={"home"}>
-      <div className="modular-row">
-
+  return <ScrollableAnchor id={"home"}>
+      <div className="modular-row" style={{justifyContent: 'center'}}>
+        <div className="card-content">
+          <p>
+            <span className="c-green">#kenny</span>
+            <span className="c-grey">·</span>&#123;
+          </p>
+          <p>
+            <span className="c-grey">··</span>
+            <span className="c-blue">position</span>:<span className="c-grey">·</span>
+            <span className="c-orange">$ui-ux-developer</span>;
+          </p>
+          <p>
+            <span className="c-grey">··</span>
+            <span className="c-blue">display</span>:<span className="c-grey">·</span>
+            <span className="c-orange">$geek</span>;
+          </p>
+          <div className="extras">
+            <p>
+              <span className="c-grey">··</span>
+              <span className="c-blue">
+                background
+              </span>:<span className="c-grey">·</span>
+              <span className="c-orange">
+                $web-dev
+              </span>,<span className="c-grey">·</span>
+              <span className="c-orange">$design</span>
+              <span className="c-grey">·</span>
+              <span className="c-blue">no-repeat</span>;
+            </p>
+            <p>
+              <span className="c-grey">··</span>
+              <span className="c-blue">clear</span>:<span className="c-grey">·</span>
+              <span className="c-blue">both</span>
+              <span className="c-red italic">!important</span>;
+            </p>
+            <p>
+              <span className="c-grey">··</span>
+              <span className="c-red">
+                &amp;
+              </span>:<span className="c-green">before</span>
+              <span className="c-grey">·</span>&#123;
+            </p>
+            <p>
+              <span className="c-grey">····</span>
+              <span className="c-blue">
+                min-height
+              </span>:<span className="c-grey">·</span>
+              <span className="c-purple">165cm</span>
+              <span className="c-grey">·</span>+<span className="c-grey">·</span>
+              <span className="c-orange">$x</span>;
+            </p>
+            <p>
+              <span className="c-grey">····</span>
+              <span className="c-blue">
+                appearance
+              </span>:<span className="c-grey">·</span>
+              <span className="c-blue">normal</span>;
+            </p>
+            <p>
+              <span className="c-grey">····</span>
+              <span className="c-blue">
+                transition-delay
+              </span>:<span className="c-grey">·</span>
+              <span className="c-purple">1980s</span>;
+            </p>
+            <p>
+              <span className="c-grey">··</span>}
+            </p>
+          </div>
+          <p>}</p>
+        </div>
+        <Scroller to="about" />
       </div>
-    </ScrollableAnchor>
-  );
+    </ScrollableAnchor>;
 };
