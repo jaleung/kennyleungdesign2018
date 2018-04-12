5ff1a13b7b2c1efe18d5b350e6fdb64bdaa395c5
Kenny <me@jaleung.net>
2018-04-12 21:44:03 +0800

    testing

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,6 @@ import Scroller from "./scrollToPage";
 // import TypeIt from 'typeit';
 import Typist from 'react-typist';
 import "../css/typist.css";
-import uuid from "uuid/v4"
 
 
 export const Home = props => {
@@ -13,96 +12,91 @@ export const Home = props => {
       <div className="modular-row" style={{ justifyContent: "center" }}>
         <Typist className="card-content" avgTypingDelay={30} cursor={{ hideWhenDone: true }}>
           <Typist.Delay ms={500} />
-          <p key={uuid()}>
-            <span className="c-green">#kenny</span>
-            <span className="c-grey">·</span>&#123;
+          <p>
+            <span className="c-green">#kenny</span><span className="c-grey">·</span><span>&#123;</span>
           </p>
           <br />
-          <p key={uuid()}>
+          <p>
             <span className="c-grey">··</span>
             <span className="c-blue">
               position
-            </span>:<span className="c-grey">·</span>
-            <span className="c-orange">$ui-ux-designer</span>;
+            </span><span>:</span><span className="c-grey">·</span>
+            <span className="c-orange">$ui-ux-designer</span><span>;</span>
             <Typist.Backspace count={10} delay={200} />
-            <span className="c-orange">-developer</span>;
+            <span className="c-orange">-developer</span><span>;</span>
           </p>
           <br />
-          <p key={uuid()}>
+          <p>
             <span className="c-grey">··</span>
             <span className="c-blue">
               display
-            </span>:<span className="c-grey">·</span>
-            <span className="c-orange">$nerd</span>;
+            </span><span>:</span><span className="c-grey">·</span>
+            <span className="c-orange">$nerd</span><span>;</span>
             <Typist.Backspace count={5} delay={200} />
-            <span className="c-orange">geek</span>;
+            <span className="c-orange">geek</span><span>;</span>
           </p>
           <br />
           <div className="extras">
-            <p key={uuid()}>
+            <p>
               <span className="c-grey">··</span>
               <span className="c-blue">
                 background
-              </span>:<span className="c-grey">·</span>
-              <span className="c-orange">
-                $web-dev
-              </span>,<span className="c-grey">·</span>
+              </span><span>:</span><span className="c-grey">·</span>
+              <span className="c-orange">$web-dev</span><span>,</span><span className="c-grey">·</span>
               <span className="c-orange">$design</span>
               <span className="c-grey">·</span>
-              <span className="c-blue">no-repeat</span>;
+              <span className="c-blue">no-repeat</span><span>;</span>
             </p>
             <br />
-            <p key={uuid()}>
+            <p>
               <span className="c-grey">··</span>
-              <span className="c-blue">
-                clear
-              </span>:<span className="c-grey">·</span>
+              <span className="c-blue">clear</span><span>:</span><span className="c-grey">·</span>
               <span className="c-blue">both</span>
-              <span className="c-red italic">!important</span>;
+              <span className="c-red italic">!important</span><span>;</span>
             </p>
             <br />
-            <p key={uuid()}>
+            <p>
               <span className="c-grey">··</span>
               <span className="c-red">
                 &amp;
-              </span>:<span className="c-green">before</span>
-              <span className="c-grey">·</span>&#123;
+              </span><span>:</span><span className="c-green">before</span>
+              <span className="c-grey">·</span><span>&#123;</span>
             </p>
             <br />
-            <p key={uuid()}>
+            <p>
               <span className="c-grey">····</span>
               <span className="c-blue">
                 min-height
-              </span>:<span className="c-grey">·</span>
+              </span><span>:</span><span className="c-grey">·</span>
               <span className="c-purple">165cm</span>
               <span className="c-grey">
                 ·
-              </span>+<span className="c-grey">·</span>
-              <span className="c-orange">$x</span>;
+              </span><span>+</span><span className="c-grey">·</span>
+              <span className="c-orange">$x</span><span>;</span>
             </p>
             <br />
-            <p key={uuid()}>
+            <p>
               <span className="c-grey">····</span>
               <span className="c-blue">
                 appearance
-              </span>:<span className="c-grey">·</span>
-              <span className="c-blue">normal</span>;
+              </span><span>:</span><span className="c-grey">·</span>
+              <span className="c-blue">normal</span><span>;</span>
             </p>
             <br />
-            <p key={uuid()}>
+            <p>
               <span className="c-grey">····</span>
               <span className="c-blue">
                 transition-delay
-              </span>:<span className="c-grey">·</span>
-              <span className="c-purple">1980s</span>;
+              </span><span>:</span><span className="c-grey">·</span>
+              <span className="c-purple">1980s</span><span>;</span>
             </p>
             <br />
-            <p key={uuid()}>
-              <span className="c-grey">··</span>}
+            <p>
+              <span className="c-grey">··</span><span>}</span> 
             </p>
             <br />
           </div>
-          <p key={uuid()}>}</p>
+          <p>}</p>
         </Typist>
         <Scroller to="about" />
       </div>
